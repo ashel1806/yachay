eb01cdab46ea640ad78852ca743a4154303beaf9
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-10-19 13:48:41 -0500

    style: cambio del mensaje de la aplicación

--- Desarrollo/OCS/Fuentes/frontend/pages/index.js
+++ Desarrollo/OCS/Fuentes/frontend/pages/index.js
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline text-green-300">
-      Hello from Docker!
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello from Docker!</h1>;
 }
