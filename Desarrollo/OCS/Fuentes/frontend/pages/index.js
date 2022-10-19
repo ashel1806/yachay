6fe2a5d860a9e729fa36db1389a7cd6c5d580b6a
Ashel Joseph Vasquez <vasquezpalominoashel@gmail.com>
2022-10-18 20:43:55 -0500

    build: implementando y configurando Docker Compose

--- Desarrollo/OCS/Fuentes/frontend/pages/index.js
+++ Desarrollo/OCS/Fuentes/frontend/pages/index.js
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-300">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline text-green-300">
+      Hello from Docker!
+    </h1>
   );
 }
