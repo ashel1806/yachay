a900dab28ac13d75dafad71eeb470ade83faa645
ElChicoAracnido10 <miguel.soria@unmsm.edu.pe>
2022-11-30 15:49:39 -0500

    fix: Se arreglo detalle Se arreglo detalle usuario y vista curso(llamen a dios por favor)

--- Desarrollo/OCS/Fuentes/frontend/pages/detalleusuario.js
+++ Desarrollo/OCS/Fuentes/frontend/pages/detalleusuario.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import Image from 'next/image';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import SearchBar from "../components/Searchbar";
 
